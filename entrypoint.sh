da018f3203e77bd794b6db090a337d6bf6769d90
amal786 <71205616+amal786@users.noreply.github.com>
2022-02-07 00:07:55 +0530

    added file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,10 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myruby/tmp/pids/server.pid
+rm -f /usr/src/app/tmp/pids/server.pid
+
+echo "bundle install..."
+bundle check || bundle install --jobs 4
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
